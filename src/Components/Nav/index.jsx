a94a6eab2060452a6b8257dac88cb4bd2aff905f
EseAlli <essealli.ea@gmail.com>
2022-05-19 17:20:33 +0100

    project component done

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -6,7 +6,9 @@ const index = () => {
       <div className="bg-transparent fixed w-full">
         <div className="flex justify-between p-3 md:py-6 md:px-8">
           <p className="text-xl md:text-2xl font-bold">ese</p>
-          <button className="border py-2 px-4 rounded-full border-black text-lg font-bold">Download Resume</button>
+          <div className="group border rounded-full border-black text-lg font-bold bg-transparent">
+            <div className="text-lg py-2 px-4 rounded-full font-bold hover:bg-black group-hover:text-[#E5E5E5] w-full">Download Resume</div>
+          </div>
         </div>
     </div>
     </div>   
--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -1,20 +1,75 @@
 import React from 'react'
-
 const index = () => {
   return (
-    <div className="text-black p-4">
-        <p className="text-4xl font-bold p-2">Projects</p>
-        <div className="p-2 overflow-x-scroll flex w-11/12">
-            <div className="h-[450px] w-[800px] border border-black">
-                <div className="">
-                    <p>Project Name</p>
-                    <p>In addition to software engineering, I also have an interest in Machine Learning. I enjoy the idea of analyzing and generating useful insights from data.</p>
+    <div className="text-white p-4 overflow-auto">
+        <p className="text-4xl text-black font-bold p-2">Projects</p>
+        <div className="p-2 pr-0 overflow-x-scroll flex">
+            <div className="h-[600px] w-[85%] border flex-none m-5">
+                <div className="project-1 h-full transition hover:delay-100 hover:filter-none group relative bg-no-repeat bg-center bg-cover hover:bg-auto">
+                    <div className="invisible group-hover:visible flex transition group-hover:delay-200 group-hover:ease-in h-full vi">
+                        <div className="m-auto border rounded-full backdrop-grayscale">
+                            <p className="p-11 py-20 font-semibold text-xl text-[#E5E5E5]">view project</p>
+                        </div>
+                    </div>
+                    <div className="absolute bottom-8 md:pl-8 w-3/12">
+                        <p className="text-2xl font-bold">Project Name</p>
+                        <p className="font-bold text-sm">In addition to software engineering, I also have an interest in Machine Learning. I enjoy the idea of analyzing and generating useful insights from data.</p>
+                        <div className ="mt-5">
+                            <span className="text-xs border border-white py-1 px-5 rounded-full mr-2">JavaScript</span>
+                            <span className="text-xs border border-white py-1 px-5 rounded-full">JavaScript</span>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="h-[450px] w-[800px] border border-black">
-                <div className="">
-                    <p>Project Name</p>
-                    <p>In addition to software engineering, I also have an interest in Machine Learning. I enjoy the idea of analyzing and generating useful insights from data.</p>
+            <div className="h-[600px] w-[85%] border flex-none m-5">
+                <div className="project-1 h-full transition hover:delay-100 hover:filter-none group relative bg-no-repeat bg-center bg-cover hover:bg-auto">
+                    <div className="hidden group-hover:flex transition group-hover:delay-200 group-hover:ease-in h-full vi">
+                        <div className="m-auto border rounded-full backdrop-grayscale">
+                            <p className="p-11 py-20 font-semibold text-xl text-[#E5E5E5]">view project</p>
+                        </div>
+                    </div>
+                    <div className="absolute bottom-8 md:pl-8 w-3/12">
+                        <p className="text-2xl font-bold">Project Name</p>
+                        <p className="font-bold text-sm">In addition to software engineering, I also have an interest in Machine Learning. I enjoy the idea of analyzing and generating useful insights from data.</p>
+                        <div className ="mt-5">
+                            <span className="text-xs border border-white py-1 px-5 rounded-full mr-2">JavaScript</span>
+                            <span className="text-xs border border-white py-1 px-5 rounded-full">JavaScript</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="h-[600px] w-[85%] border flex-none m-5">
+                <div className="project-1 h-full transition hover:delay-100 hover:filter-none group relative bg-no-repeat bg-center bg-cover hover:bg-auto">
+                    <div className="hidden group-hover:flex transition group-hover:delay-200 group-hover:ease-in h-full vi">
+                        <div className="m-auto border rounded-full backdrop-grayscale">
+                            <p className="p-11 py-20 font-semibold text-xl text-[#E5E5E5]">view project</p>
+                        </div>
+                    </div>
+                    <div className="absolute bottom-8 md:pl-8 w-3/12">
+                        <p className="text-2xl font-bold">Project Name</p>
+                        <p className="font-bold text-sm">In addition to software engineering, I also have an interest in Machine Learning. I enjoy the idea of analyzing and generating useful insights from data.</p>
+                        <div className ="mt-5">
+                            <span className="text-xs border border-white py-1 px-5 rounded-full mr-2">JavaScript</span>
+                            <span className="text-xs border border-white py-1 px-5 rounded-full">JavaScript</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="h-[600px] w-[85%] border flex-none m-5">
+                <div className="project-1 h-full transition hover:delay-100 hover:filter-none group relative bg-no-repeat bg-center bg-cover hover:bg-auto">
+                    <div className="hidden group-hover:flex transition group-hover:delay-200 group-hover:ease-in h-full vi">
+                        <div className="m-auto border rounded-full backdrop-grayscale">
+                            <p className="p-11 py-20 font-semibold text-xl text-[#E5E5E5]">view project</p>
+                        </div>
+                    </div>
+                    <div className="absolute bottom-8 md:pl-8 w-3/12">
+                        <p className="text-2xl font-bold">Project Name</p>
+                        <p className="font-bold text-sm">In addition to software engineering, I also have an interest in Machine Learning. I enjoy the idea of analyzing and generating useful insights from data.</p>
+                        <div className ="mt-5">
+                            <span className="text-xs border border-white py-1 px-5 rounded-full mr-2">JavaScript</span>
+                            <span className="text-xs border border-white py-1 px-5 rounded-full">JavaScript</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
